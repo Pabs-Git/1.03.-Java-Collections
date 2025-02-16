f2dfbbaa18040bb45600a17c91e368e0e81c3091
Pablo Orbea <pabloorbea1@gmail.com>
2025-02-16 23:18:25 +0100

    .

---
+++ Nivel_1/src/Ejercicio_1/classes/Month.java
@@ -0,0 +1,33 @@
+package Ejercicio_1.classes;
+
+public class Month {
+
+    private String name;
+
+    public Month(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Month month = (Month) obj;
+        return name.equals(month.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+    @Override
+    public String toString() {
+        return "month=" + name;
+    }
+}
